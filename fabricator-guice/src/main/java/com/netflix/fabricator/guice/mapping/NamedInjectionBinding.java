67a392f7a7b88061c568fc93f267dd7241d10db5
elandau <elandau@yahoo.com>
2014-05-22 00:17:29 -0700

    Property binding support for named bindings with generics

--- fabricator-guice/src/main/java/com/netflix/fabricator/guice/mapping/NamedInjectionBinding.java
+++ fabricator-guice/src/main/java/com/netflix/fabricator/guice/mapping/NamedInjectionBinding.java
@@ -7,6 +7,7 @@ import com.google.inject.name.Names;
 import com.netflix.fabricator.ConfigurationNode;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 
 /**
  * Created by hyuan on 1/16/14.
@@ -15,7 +16,17 @@ public class NamedInjectionBinding implements BindingReslove {
 
     @Override
     public boolean execute(String name, Object obj, ConfigurationNode config, Class<?> argType, Injector injector, Method method) throws Exception {
-        Binding<?> binding = injector.getExistingBinding(Key.get(argType, Names.named(name)));
+        
+        Binding<?> binding;
+        
+        Type pType = method.getGenericParameterTypes()[0];
+        if (pType != null) {
+            binding = injector.getExistingBinding(Key.get(pType, Names.named(name)));
+        }
+        else {
+            binding = injector.getExistingBinding(Key.get(argType, Names.named(name)));
+        }
+        
         if (binding != null) {
             method.invoke(obj, binding.getProvider().get());
             return true;
